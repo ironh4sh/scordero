bdf432626ba002f57ff35155d309a7faed60c127
ironh4sh <ironh4sh@gmail.com>
2026-05-28 12:00:25 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,103 @@
+
+
+function toggleMenu(){
+    document.getElementById("mobileMenu")
+    .classList.toggle("active");
+}
+
+
+
+/* =========================
+Ini: Scroll Top
+========================= */
+const scrollTopBtn = document.getElementById('scrollTop');
+
+const expandBtns = document.querySelectorAll('.expand-btn');
+
+expandBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const card = btn.parentElement;
+        card.classList.toggle('active');
+    });
+});
+
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        scrollTopBtn.classList.add('show');
+    } else {
+        scrollTopBtn.classList.remove('show');
+    }
+});
+
+scrollTopBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+/* =========================
+End: Scroll Top
+========================= */
+
+
+/* =========================
+Privacy Modal
+========================= */
+
+const openPrivacyBtn = document.getElementById("openPrivacyModal");
+const closePrivacyBtn = document.getElementById("closePrivacyModal");
+const privacyModal = document.getElementById("privacyModal");
+
+if(openPrivacyBtn && privacyModal){
+    openPrivacyBtn.addEventListener("click", () => {
+        privacyModal.classList.add("active");
+    });
+}
+
+if(closePrivacyBtn && privacyModal){
+    closePrivacyBtn.addEventListener("click", () => {
+        privacyModal.classList.remove("active");
+    });
+}
+
+/* =========================
+Legal Modal 
+========================= */
+
+const openLegal = document.getElementById("openLegalModal");
+const closeLegal = document.getElementById("closeLegalModal");
+const legalModal = document.getElementById("legalModal");
+
+if(openLegal && legalModal){
+    openLegal.addEventListener("click", function(e){
+        e.preventDefault();
+        legalModal.classList.add("active");
+    });
+}
+
+if(closeLegal && legalModal){
+    closeLegal.addEventListener("click", () => {
+        legalModal.classList.remove("active");
+    });
+}
+
+
+/* =========================
+Issue Modal
+========================= */
+const openIssueBtn = document.getElementById("openIssueModal");
+const closeIssueBtn = document.getElementById("closeIssueModal");
+const issueModal = document.getElementById("issueModal");
+
+if(openIssueBtn && issueModal){
+    openIssueBtn.addEventListener("click", () => {
+        issueModal.classList.add("active");
+    });
+}
+
+if(closeIssueBtn && issueModal){
+    closeIssueBtn.addEventListener("click", () => {
+        issueModal.classList.remove("active");
+    });
+}
